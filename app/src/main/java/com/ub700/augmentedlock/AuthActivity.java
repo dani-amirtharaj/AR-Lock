1e38e81341177d7ecc55e664abac5466f4889f63
Daniel Amirtharaj <dani.amirtharaj@gmail.com>
2019-04-24 00:45:48 -0400

    Latest changes

--- app/src/main/java/com/ub700/augmentedlock/AuthActivity.java
+++ app/src/main/java/com/ub700/augmentedlock/AuthActivity.java
@@ -8,17 +8,20 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
@@ -26,23 +29,33 @@ import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.HitTestResult;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
-import com.google.ar.sceneform.rendering.Material;
 import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
-import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.FootprintSelectionVisualizer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 
 public class AuthActivity extends AppCompatActivity {
 
     private static final String TAG = AuthActivity.class.getSimpleName();
+
     private static final double MIN_OPENGL_VERSION = 3.0;
+    private static final int CHAR_LIMIT = 8;
+    private static final int NUM_RETRIES = 5;
+    private static final int NUM_BUTTONS = 9;
+    private static final int HIT_WAIT = 15;
 
     private ArFragment arFragment;
     private Scene scene;
+    private AnchorNode anchorNode;
     private static int deviceHeight;
     private static int deviceWidth;
     private boolean firstSelectionDelay;
@@ -50,24 +63,24 @@ public class AuthActivity extends AppCompatActivity {
     private Node selectedNode;
     private Node previousSelection;
     private ImageView buttonSelectorView;
-    private Material buttonMaterial;
     private static int buttonColor;
     private ValueAnimator buttonAnimator;
-    private ModelRenderable[] buttons;
     private boolean isKeypadRendered;
     private String key;
     private StringBuilder enteredKey;
-    private static final int NUM_BUTTONS = 9;
-    private static final int HIT_WAIT = 15;
+    private Toast wrongKeyToast;
+    private int retries;
+
+    private List<CompletableFuture<ModelRenderable>> buttonCompletable = new ArrayList<>();
+    private List<ModelRenderable> buttons = new ArrayList<>();
 
 
     public AuthActivity() {
         hitCount = 0;
         isKeypadRendered = false;
-        firstSelectionDelay = false;
-        buttons = new ModelRenderable[9];
+        firstSelectionDelay = true;
         enteredKey = new StringBuilder();
-        buttonColor = android.graphics.Color.argb(255,89, 139, 214);
+        buttonColor = android.graphics.Color.argb(1,0.06f,0.06f, 0.85f);
     }
 
 
@@ -80,25 +93,26 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
-        buildButtonMaterial(buttonColor, 1, 1);
-
         Intent intent = getIntent();
         key = intent.getExtras().getString("Key");
 
         scene = arFragment.getArSceneView().getScene();
         buttonSelectorView = findViewById(R.id.button_selector);
         setDisplayHeightWidth();
+        if (!buildButtons()) {
+            Log.e(TAG, "Error loading Buttons!");
+        }
 
         /* Plane listener to allow user to place keypad on surface of their choice.*/
         arFragment.setOnTapArPlaneListener(
                 (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    if (!buildButtons()) {
+                    if (buttons.size() != NUM_BUTTONS) {
                         return;
                     }
                     if (!isKeypadRendered) {
                         Anchor anchor = hitResult.createAnchor();
-                        AnchorNode anchorNode = new AnchorNode(anchor);
-                        anchorNode.setParent(arFragment.getArSceneView().getScene());
+                        anchorNode = new AnchorNode(anchor);
+                        anchorNode.setParent(scene);
 
                         buildKeypad(anchorNode);
                         this.runOnUiThread(() -> {
@@ -117,11 +131,14 @@ public class AuthActivity extends AppCompatActivity {
                 /* To add delay the before the first selection. */
                 if (firstSelectionDelay) {
                     hitCount++;
-                    if (hitCount > HIT_WAIT+5) {
+                    if (hitCount > HIT_WAIT + 5) {
                         hitCount = 0;
                         firstSelectionDelay = false;
                     }
                 } else {
+                    if (wrongKeyToast != null) {
+                        wrongKeyToast.cancel();
+                    }
                     /* HitTestResult holds any node in the scene hit by a ray. */
                     HitTestResult hitTestResult = scene.hitTest(scene.getCamera().screenPointToRay(deviceWidth / 2, deviceHeight / 2));
                     if (hitTestResult != null && hitTestResult.getNode() != null) {
@@ -142,13 +159,18 @@ public class AuthActivity extends AppCompatActivity {
                                 previousSelection.getRenderable().getMaterial().setFloat3(MaterialFactory.MATERIAL_COLOR, new Color(buttonColor));
                             }
                             previousSelection = selectedNode;
-                            buttonAnimation(selectedNode);
+                            buttonAnimation(selectedNode, android.graphics.Color.GREEN);
 
                             /* Get selection and compare to actual key. */
                             enteredKey.append(selectedNode.getName());
-                            Log.e(TAG, enteredKey.toString());
+                            Log.e(TAG, "Entered Key: "+enteredKey.toString());
                             if (enteredKey.toString().equals(key)) {
-                                startNextActivity();
+                                for (Node node : anchorNode.getChildren()) {
+                                    buttonAnimation(node, android.graphics.Color.GREEN);
+                                }
+                                startNextActivity(true);
+                            } else if (enteredKey.length() >= CHAR_LIMIT) {
+                                resetKey();
                             }
                             hitCount = 0;
                         }
@@ -158,26 +180,36 @@ public class AuthActivity extends AppCompatActivity {
         });
     }
 
-    /* Building material to be applied to buttons. */
-    private void buildButtonMaterial(int color, float metallic, float reflectance) {
-        MaterialFactory.makeOpaqueWithColor(this, new Color(color))
-                .thenAccept(
-                        material -> {
-                            material.setFloat(MaterialFactory.MATERIAL_METALLIC, metallic);
-                            material.setFloat(MaterialFactory.MATERIAL_REFLECTANCE, reflectance);
-                            buttonMaterial = material;
-                        });
-    }
-
     /* Build all buttons for keypad. */
     private boolean buildButtons() {
-        if (buttonMaterial == null) {
-            return false;
+        for (int i = 1; i <= NUM_BUTTONS; i++) {
+            try {
+                buttonCompletable.add(i-1, ModelRenderable.builder()
+                        .setSource(this, Uri.parse("button" + i + ".sfb"))
+                        .build());
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
         }
-        for (int i = 0; i < NUM_BUTTONS; i++) {
-            buttons[i] = ShapeFactory.makeCylinder(0.07f, 0.05f,
-                    new Vector3(0.0f, 0.15f, 0.0f),  buttonMaterial.makeCopy());
-        }
+
+        CompletableFuture.allOf((CompletableFuture<ModelRenderable>[]) buttonCompletable.toArray(new CompletableFuture[buttonCompletable.size()]))
+                .handle((notUsed, throwable) -> {
+                            if (throwable != null) {
+                                return null;
+                            }
+                            for (int i = 0; i < NUM_BUTTONS; i++) {
+
+                                try {
+                                    Log.e(TAG, "Here" + i);
+                                    ModelRenderable tempButton = buttonCompletable.get(i).get();
+//                                    tempButton.getMaterial(0).setFloat4("baseColor", 1f, 1f, 1f, 0.8f);
+                                    buttons.add(i, tempButton);
+                                } catch (InterruptedException | ExecutionException ex) {
+                                    Log.e(TAG, ex.toString());
+                                }
+                            }
+                            return null;
+                        });
         return true;
     }
 
@@ -187,16 +219,19 @@ public class AuthActivity extends AppCompatActivity {
         int index = 0;
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
-                localPosition.set(0.25f * j, 0.0f, 0.25f * i);
+                localPosition.set(0.3f * j, 0.0f, 0.3f * i);
                 buttonNode = new Node();
                 buttonNode.setParent(anchorNode);
                 buttonNode.setLocalPosition(localPosition);
-                buttonNode.setRenderable(buttons[index]);
+                buttonNode.setLocalRotation(Quaternion.lookRotation(new Vector3(0, 1, 0), new Vector3(0, 0, -1f)));
+                buttonNode.setWorldScale(new Vector3(3, 3, 3));
+                buttonNode.setRenderable(buttons.get(index));
                 buttonNode.setName(Integer.toString(index + 1));
                 buttonNode.setOnTapListener(((HitTestResult hitTestResult, MotionEvent motionEve) -> {
                     if (hitTestResult != null || hitTestResult.getNode() != null)
-                        hitTestResult.getNode().getRenderable().getMaterial().setFloat3(MaterialFactory.MATERIAL_COLOR,
-                                new Color(android.graphics.Color.RED));
+//                        hitTestResult.getNode().getRenderable().getMaterial(1).setFloat3("baseColor",
+//                                new Color(android.graphics.Color.RED));
+                        resetKey();
                 }));
                 index++;
             }
@@ -219,27 +254,50 @@ public class AuthActivity extends AppCompatActivity {
     }
 
     /* Animation to show button selection. */
-    private void buttonAnimation(Node selectedNode) {
+    private void buttonAnimation(Node selectedNode, int color) {
         buttonAnimator = new ValueAnimator();
-        buttonAnimator.setIntValues(android.graphics.Color.GREEN, buttonColor);
+        buttonAnimator.setIntValues(color, buttonColor);
         buttonAnimator.setEvaluator(new ArgbEvaluator());
         buttonAnimator.setInterpolator(new LinearInterpolator());
+        if (selectedNode == null || selectedNode.getRenderable() == null || selectedNode.getRenderable().getMaterial(1) == null) {
+            return;
+        }
         buttonAnimator.addListener(new AnimatorListenerAdapter() {
             public void onAnimationEnd(Animator animation) {
-                selectedNode.getRenderable().getMaterial().setFloat3(MaterialFactory.MATERIAL_COLOR, new Color(buttonColor));
+                selectedNode.getRenderable().getMaterial(1).setFloat3("baseColor", new Color(buttonColor));
             }});
         buttonAnimator.addUpdateListener((ValueAnimator valueButtonAnimatorator) -> {
-            selectedNode.getRenderable().getMaterial().setFloat3(MaterialFactory.MATERIAL_COLOR,
+            selectedNode.getRenderable().getMaterial(1).setFloat3("baseColor",
                     new Color((int) valueButtonAnimatorator.getAnimatedValue()));
         });
         buttonAnimator.setDuration(150);
         buttonAnimator.start();
     }
 
+    private void resetKey() {
+        retries++;
+        if (retries == NUM_RETRIES) {
+            startNextActivity(false);
+        }
+        for (Node node : anchorNode.getChildren()) {
+            buttonAnimation(node, android.graphics.Color.RED);
+        }
+        if (enteredKey.length() >= CHAR_LIMIT) {
+            wrongKeyToast = Toast.makeText(this, "Wrong key entered!", Toast.LENGTH_SHORT);
+        } else {
+            wrongKeyToast = Toast.makeText(this, "Key reset!", Toast.LENGTH_SHORT);
+        }
+        enteredKey.delete(0, enteredKey.length());
+        wrongKeyToast.setGravity(Gravity.BOTTOM, 0, 0);
+        wrongKeyToast.show();
+        firstSelectionDelay = true;
+    }
+
     /* Start next activity. */
-    private void startNextActivity() {
-        Intent intent2 = new Intent(getApplicationContext(), WelcomeActivity.class);
-        startActivity(intent2);
+    private void startNextActivity(boolean authStatus) {
+        Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+        intent.putExtra("authStatus", authStatus);
+        startActivity(intent);
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
         finish();
     }
--- app/src/main/java/com/ub700/augmentedlock/WelcomeActivity.java
+++ app/src/main/java/com/ub700/augmentedlock/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.ub700.augmentedlock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -9,6 +11,15 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        boolean authStatus = intent.getExtras().getBoolean("authStatus");
         setContentView(R.layout.activity_welcome);
+
+        TextView textView = findViewById(R.id.textView);
+        if (authStatus) {
+            textView.setText(R.string.welcome);
+        } else {
+            textView.setText(R.string.manyRetires);
+        }
     }
 }
